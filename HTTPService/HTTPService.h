a1f025d0a5016283c65d2d0cb64d69e0193e5325
Jeremy Fox <atljeremy@me.com>
2015-04-05 12:58:42 -0400

    Initial Commit

---
+++ HTTPService/HTTPService.h
@@ -0,0 +1,19 @@
+//
+//  HTTPService.h
+//  HTTPService
+//
+//  Created by Jeremy Fox on 4/5/15.
+//  Copyright (c) 2015 Jeremy Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTTPService.
+FOUNDATION_EXPORT double HTTPServiceVersionNumber;
+
+//! Project version string for HTTPService.
+FOUNDATION_EXPORT const unsigned char HTTPServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPService/PublicHeader.h>
+
+
