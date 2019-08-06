b8e217a5cb37c2cce45795266cc07b4e5810df64
Jeremy Fox <atljeremy@me.com>
2019-08-06 15:27:38 -0400

    Added macOS target

---
+++ HTTPService macOS/HTTPService_macOS.h
@@ -0,0 +1,19 @@
+//
+//  HTTPService_macOS.h
+//  HTTPService macOS
+//
+//  Created by Jeremy Fox on 8/6/19.
+//  Copyright © 2019 Jeremy Fox. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HTTPService_macOS.
+FOUNDATION_EXPORT double HTTPService_macOSVersionNumber;
+
+//! Project version string for HTTPService_macOS.
+FOUNDATION_EXPORT const unsigned char HTTPService_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPService_macOS/PublicHeader.h>
+
+
